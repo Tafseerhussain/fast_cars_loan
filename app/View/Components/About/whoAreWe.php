6d343f756e1810f42a7cf3ad18b3ffbce51fab5a
Tafseer <tafseerhussain88@gmail.com>
2022-12-24 20:59:15 +0500

    who we are

--- app/View/Components/About/whoAreWe.php
+++ app/View/Components/About/whoAreWe.php
@@ -7,28 +7,13 @@ use App\Models\AboutPage;
 
 class whoAreWe extends Component
 {
-    public $who;
+    public $whoWeAre;
 
     public function __construct()
     {
-        $this->who = AboutPage::where('id', 1)->first(
-            [
-                'who_head',
-                'who_text', 
-                'who_img1', 
-                'who_img2', 
-                'who_img3',
-                'who_img4', 
-                'who_hidden'
-            ]
-        );
+        $this->whoWeAre = AboutPage::where('id', 1)->first(['who_head', 'who_text', 'who_img1', 'who_img2', 'who_img3','who_img4', 'who_hidden']);
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\Contracts\View\View|\Closure|string
-     */
     public function render()
     {
         return view('components.about.who-are-we');
--- resources/views/components/about/who-are-we.blade.php
+++ resources/views/components/about/who-are-we.blade.php
@@ -1,4 +1,4 @@
-@if ($who->who_hidden == 0)
+@if ($whoWeAre->who_hidden == 0)
 <div class="who-we-are">
     <img src="{{ asset('img/about/who-left.png') }}" class="who-left" alt="circle lines">
     <img src="{{ asset('img/about/who-right.png') }}" class="who-right" alt="pipe lines">
@@ -6,30 +6,30 @@
         <div class="row">
             <div class="col-md-8">
                 <h1 class="section-title">
-                    {{ $who->who_head }}
+                    {{ $whoWeAre->who_head }}
                 </h1>
                 <p>
-                    {!! $who->who_text !!}
+                    {!! $whoWeAre->who_text !!}
                 </p>
                 <div class="row">
                     <div class="col-md-7">
                         <div class="img-box-1">
-                            <img src="{{ asset($who->who_img1) }}" alt="who we are">
+                            <img src="{{ asset($whoWeAre->who_img1) }}" alt="who we are">
                         </div>
                     </div>
                     <div class="col-md-5">
                         <div class="img-box-2">
-                            <img src="{{ asset($who->who_img2) }}" alt="who we are">
+                            <img src="{{ asset($whoWeAre->who_img2) }}" alt="who we are">
                         </div>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="img-box-3">
-                    <img src="{{ asset($who->who_img3) }}" alt="who we are">
+                    <img src="{{ asset($whoWeAre->who_img3) }}" alt="who we are">
                 </div>
                 <div class="img-box-4">
-                    <img src="{{ asset($who->who_img4) }}" alt="who we are">
+                    <img src="{{ asset($whoWeAre->who_img4) }}" alt="who we are">
                 </div>
             </div>
         </div>
