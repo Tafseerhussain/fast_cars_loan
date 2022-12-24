b51b5d3b4cf244cef5c7b7f8b9fef7ee5da0ab93
Tafseer <tafseerhussain88@gmail.com>
2022-12-24 21:00:57 +0500

    Update whoAreWe.php

--- app/View/Components/About/whoAreWe.php
+++ app/View/Components/About/whoAreWe.php
@@ -7,15 +7,14 @@ use App\Models\AboutPage;
 
 class whoAreWe extends Component
 {
-    public $whoWeAre;
-
     public function __construct()
     {
-        $this->whoWeAre = AboutPage::where('id', 1)->first(['who_head', 'who_text', 'who_img1', 'who_img2', 'who_img3','who_img4', 'who_hidden']);
+        //
     }
 
     public function render()
     {
-        return view('components.about.who-are-we');
+        $whoWeAre = AboutPage::where('id', 1)->first(['who_head', 'who_text', 'who_img1', 'who_img2', 'who_img3','who_img4', 'who_hidden']);
+        return view('components.about.who-are-we', compact('whoWeAre'));
     }
 }
